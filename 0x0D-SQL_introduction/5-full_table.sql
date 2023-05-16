a9ddfe0193de15293c88dbedd480075d0dbac06a
ifouzezi <ifoghaleuzezi@gmail.com>
2023-05-16 12:28:04 +0100

    Update 5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,11 @@
 -- Prints the full description of the table first_table.
-SHOW CREATE TABLE `first_table`;
+SELECT
+    CONCAT('CREATE TABLE ', TABLE_NAME, ' (', GROUP_CONCAT(COLUMN_NAME, ' ', COLUMN_TYPE), ');') AS 'Create Table'
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'your_database_name' AND
+    TABLE_NAME = 'first_table'
+GROUP BY
+    TABLE_NAME;
+
