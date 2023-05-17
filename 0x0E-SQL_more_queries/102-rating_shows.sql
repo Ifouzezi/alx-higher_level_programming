96788aace286099e310c603f20efe69de4ef27f4
ifouzezi <ifoghaleuzezi@gmail.com>
2023-05-17 14:10:25 +0100

    try

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -4,5 +4,5 @@ SELECT `title`, SUM(`rate`) AS `rating`
   FROM `tv_shows` AS t
        INNER JOIN `tv_show_ratings` AS r
        ON t.`id` = r.`show_id`
- GROUP BY `title`
+ GROUP BY title
  ORDER BY `rating` DESC;
