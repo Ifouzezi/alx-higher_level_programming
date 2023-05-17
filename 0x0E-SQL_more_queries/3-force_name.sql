27c9f4be71ea80511c221020068f23fee517345f
ifouzezi <ifoghaleuzezi@gmail.com>
2023-05-17 14:01:50 +0100

    Update 3-force_name.sql

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
--- Creates the table force_name.
-CREATE TABLE IF NOT EXISTS `force_name` (
-    `id`   INT,
-    `name` VARCHAR(256) NOT NULL
+-- Creates the table force_name if it doesn't exist
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
 );
