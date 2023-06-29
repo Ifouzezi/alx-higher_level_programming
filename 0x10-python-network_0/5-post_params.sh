1b31a23cebb5e8dac7f2a53cc6b2415ad7b1c8d6
ifouzezi <ifoghaleuzezi@gmail.com>
2023-06-29 12:59:58 +0100

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-### Sends POST request with variables, displays body of response
-curl "$@" -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD"
+# Bash scripts that sends a POST request to a given URL.
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
