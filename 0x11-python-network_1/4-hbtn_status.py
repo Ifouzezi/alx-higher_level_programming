211481ed65ba298ba8005291924ed6f9dae0c31d
ifouzezi <ifoghaleuzezi@gmail.com>
2023-06-29 14:24:44 +0100

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
-import requests
-
-
+""" Fetches website info using requests (must be installed)
+"""
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    import requests
     print("Body response:")
+    r = requests.get('https://intranet.hbtn.io/status')
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
     
