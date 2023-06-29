6a8f8dd1680cb2b8f7f44109751a2ef804b9ccf9
ifouzezi <ifoghaleuzezi@gmail.com>
2023-06-29 12:16:19 +0100

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-# Set the URL
-url="$1"
-
-# Send the POST request using curl and display the response body
-response=$(curl -sX POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$url")
-echo "$response"
+### Sends POST request with variables, displays body of response
+curl "$@" -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD"
