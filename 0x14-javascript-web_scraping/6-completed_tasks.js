8fb47535b23aa4bb54953ab534c104d1d28f3632
ifouzezi <ifoghaleuzezi@gmail.com>
2023-07-25 12:52:58 +0100

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,15 +1,47 @@
 #!/usr/bin/node
 const request = require('request');
 
-if (process.argv.length > 2) {
-  request(process.argv[2], (error, response, responseBody) => {
-    const userCompletionCount = {};
-
+function fetchAllData(url, callback) {
+  request(url, (error, response, responseBody) => {
     if (error) {
       console.log(error);
+      callback(error, null);
+      return;
     }
 
-    JSON.parse(responseBody).forEach(item => {
+    const data = JSON.parse(responseBody);
+    if (data.length === 0) {
+      // No more data, invoke the callback with the collected results
+      callback(null, data);
+    } else {
+      // Continue fetching data from the next page
+      const nextPageUrl = response.headers.link.match(/<([^>]+)>;\s*rel="next"/);
+      if (nextPageUrl) {
+        fetchAllData(nextPageUrl[1], (err, restOfData) => {
+          if (err) {
+            callback(err, null);
+            return;
+          }
+          callback(null, data.concat(restOfData));
+        });
+      } else {
+        // This is the last page, invoke the callback with the collected results
+        callback(null, data);
+      }
+    }
+  });
+}
+
+if (process.argv.length > 2) {
+  const apiUrl = process.argv[2];
+  fetchAllData(apiUrl, (err, allData) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const userCompletionCount = {};
+    allData.forEach(item => {
       if (item.completed) {
         if (!userCompletionCount[item.userId]) {
           userCompletionCount[item.userId] = 0;
