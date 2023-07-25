aeb2df61adab1cfe797b03e4099fb78e71d13f3f
ifouzezi <ifoghaleuzezi@gmail.com>
2023-07-25 13:00:34 +0100

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -8,14 +8,14 @@ request(URL, async function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    // Takes results and deserializes from JSON to js obj
-    // console.log(Object.prototype.toString.call(array)) to confirm obj type
+    // Takes results and deserializes from JSON to JS object
+    // console.log(Object.prototype.toString.call(characterURLs)) to confirm object type
 
-    const array = JSON.parse(body).characters;
+    const characterURLs = JSON.parse(body).characters;
 
-    // Use list of links to character pages to make new requests
+    // Use a list of links to character pages to make new requests
     // await queues requests until they resolve in order
-    for (const characterURL of array) {
+    for (const characterURL of characterURLs) {
       await new Promise(function (resolve, reject) {
         request(characterURL, function (error, response, body) {
           if (error) {
@@ -27,8 +27,7 @@ request(URL, async function (error, response, body) {
             resolve();
           }
         });
-      }
-      );
+      });
     }
   }
 });
