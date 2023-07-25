70947ef46316b9ad23a44fe39725dede9c5c1313
ifouzezi <ifoghaleuzezi@gmail.com>
2023-07-25 12:58:21 +0100

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,29 +1,22 @@
 #!/usr/bin/node
 const request = require('request');
-const movieID = process.argv[2];
-const URL = 'http://swapi.co/api/films/' + movieID;
+const BASE_URL = 'https://swapi-api.hbtn.io/api';
 
-// Makes API request, will print error object if occurs
-request(URL, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    // Takes results and deserializes from JSON to js obj
-    // console.log(Object.prototype.toString.call(array)) to confirm obj type
-
-    const array = JSON.parse(body).characters;
-
-    // Use list of links to character pages to make new requests
-    for (const characterURL of array) {
-      request(characterURL, function (error, response, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          // Extract character names
-          const characterName = JSON.parse(body).name;
-          console.log(characterName);
-        }
-      });
+if (process.argv.length > 2) {
+  request(`${BASE_URL}/films/${process.argv[2]}/`, (error, response, responseBody) => {
+    if (error) {
+      console.log(error);
     }
-  }
-});
+    const charactersURLs = JSON.parse(responseBody).characters;
+
+    charactersURLs.forEach(characterURL => {
+      request(characterURL, (err, res, body) => {
+        if (err) {
+          console.log(err);
+        }
+        const characterName = JSON.parse(body).name;
+        console.log(characterName);
+      });
+    });
+  });
+}
